6271122e15eccd4d09409e817800b9a368c9bf77
Christian Hettlage <hettlage@saao.ac.za>
2023-03-28 21:39:56 +0200

    Add initial plugin file

---
+++ saao-scm.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: saao-scm
+ * Plugin URI: https://www.saao.ac.za/
+ * Description: WordPress plugin for the SAAO's SCM sites.
+ * Version: 0.1
+ * Author: Christian Hettlage (SAAO/SALT)
+ * Author URI: https://www.saao.ac.za/
+ **/
